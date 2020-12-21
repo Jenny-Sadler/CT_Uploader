cf5cc7af83f6e4968f6dfae1e3318da20887cc22
Jenny Sadler <jenny.sadler@ons.gov.uk>
2020-12-21 18:46:01 +0000

    Updating lambda file to reflect lambda function in AWS account

--- lambda.js
+++ lambda.js
@@ -2,34 +2,32 @@ const AWS = require("aws-sdk");
 AWS.config.update({region: process.env.REGION || "eu-west-2"})
 const s3 = new AWS.S3();
 
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
   const result = await getUploadURL(event);
-  console.log("Result : " + result)
   return result;
 }
 
 const getUploadURL = async (event) => {
-  console.log(event);
-  const randomId = parseInt(Math.random() * 10000000)
   const s3Params = {
     Bucket: 'council-tax-upload-ons',
-    Key:  `${randomId}.csv`,
-    ContentType: 'text/csv',
+    Key:  event.queryStringParameters.filename,
+    ContentType: "text/csv",
     Expires: 60,
     ACL: 'public-read'
   }
+  console.log("filename: " + event.filename)
   return new Promise((resolve, reject) => {
     let uploadURL = s3.getSignedUrl('putObject', s3Params)
     resolve({
       "statusCode": 200,
       "isBase64Encoded": false,
       "headers": { 'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': '*',
+                    //'Access-Control-Allow-Headers': '*',
+                    //'Access-Control-Allow-Methods': 'GET, OPTIONS'
          },
       "body": JSON.stringify({
         "uploadURL": uploadURL,
-        "filename": `${randomId}.csv`,
-        "event": event
+        "filename": event.queryStringParameters.filename
       })
     })
   })
